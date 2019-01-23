0a43a377cb2bca716673b6a210a555aeedb8fb2e
Michellealive <michellealive1309@gmail.com>
2019-01-23 15:17:56 +0700

    Finish Home Page

--- javascript/main.js
+++ javascript/main.js
@@ -20,30 +20,3 @@ $(function() {
         $("#portipat").css("opacity", 1 - $(window).scrollTop() / 700)
     });
 });
-
-var stars = document.getElementById('stars')
-var star = document.getElementsByClassName('star')
-
-
-for (var j = 0; j < 30; j++) {
-    var newStar = document.createElement("div")
-    newStar.className = "star"
-    newStar.style.top = randomDistance(30, -30) + 'px'
-    newStar.style.left = randomDistance(150, 20) + 'px'
-    stars.appendChild(newStar)
-}
-
-
-function randomDistance(max, min) {
-    var distance = Math.floor(Math.random() * (max - min + 1) * 10 + min)
-    return distance
-}
-
-
-for (var i = 0, len = star.length; i < len; i++) {
-    if (i % 6 == 0) {
-        star[i].style.animationDelay = '0s'
-    } else {
-        star[i].style.animationDelay = i * 0.8 + 's'
-    }
-}
