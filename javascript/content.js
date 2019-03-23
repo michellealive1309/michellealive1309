1442b3b02cdce14f8a298e4cb159be96d905196e
Michellealive <michellealive1309@gmail.com>
2019-03-23 14:11:19 +0700

    NewWebPort
    Reset all web.

--- javascript/content.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.cerinfo').on('mousemove', function(e){
-        centerX = $(this).width()/2;
-        var moveX = centerX - e.offsetX;
-        $(this).css({
-            'transform': 'perspective(500px) rotateY(' + moveX/20 + 'deg)'
-        });
-    });
-});
